dc819dd06684c9a9717139a18200d192fd449bf5
Martin Kaip <16665420+kaipm@users.noreply.github.com>
2022-06-03 22:52:02 +0200

    Convert to file scoped namespace

--- src/TrendFox.Blazor.Regions/Components/Region.razor.cs
+++ src/TrendFox.Blazor.Regions/Components/Region.razor.cs
@@ -1,43 +1,42 @@
 using Microsoft.AspNetCore.Components;
 
-namespace TrendFox.Blazor.Regions
+namespace TrendFox.Blazor.Regions;
+
+/// <summary>
+/// Defines a region to render registered components.
+/// </summary>
+public partial class Region
 {
     /// <summary>
-    /// Defines a region to render registered components.
+    /// The name of the region used to register components.
     /// </summary>
-    public partial class Region
+    [Parameter, EditorRequired]
+    public string Name { get; set; } = "";
+
+    /// <summary>
+    /// The template used to render registered components.
+    /// </summary>
+    [Parameter]
+    public RenderFragment<RenderFragment>? ComponentTemplate { get; set; }
+
+    ///<inheritdoc/>
+    protected override void OnInitialized()
     {
-        /// <summary>
-        /// The name of the region used to register components.
-        /// </summary>
-        [Parameter, EditorRequired]
-        public string Name { get; set; } = "";
+        base.OnInitialized();
+        RegionRegistry.RegionChanged += RegionRegistry_RegionChanged;
+    }
 
-        /// <summary>
-        /// The template used to render registered components.
-        /// </summary>
-        [Parameter]
-        public RenderFragment<RenderFragment>? ComponentTemplate { get; set; }
+    ///<inheritdoc/>
+    public void Dispose()
+    {
+        RegionRegistry.RegionChanged -= RegionRegistry_RegionChanged;
+    }
 
-        ///<inheritdoc/>
-        protected override void OnInitialized()
+    private void RegionRegistry_RegionChanged(object? sender, RegionChangedEventArgs e)
+    {
+        if (e.Regions.Length == 0 || e.Regions.Contains(Name))
         {
-            base.OnInitialized();
-            RegionRegistry.RegionChanged += RegionRegistry_RegionChanged;
-        }
-
-        ///<inheritdoc/>
-        public void Dispose()
-        {
-            RegionRegistry.RegionChanged -= RegionRegistry_RegionChanged;
-        }
-
-        private void RegionRegistry_RegionChanged(object? sender, RegionChangedEventArgs e)
-        {
-            if (e.Regions.Length == 0 || e.Regions.Contains(Name))
-            {
-                StateHasChanged();
-            }
+            StateHasChanged();
         }
     }
 }
