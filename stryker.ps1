1a8635c1d0b24e11029fe408f7774c97d229f295
Martin Kaip <16665420+kaipm@users.noreply.github.com>
2022-06-05 15:38:17 +0200

    Fix stryker script for linux, add pwsh hint

--- stryker.ps1
+++ stryker.ps1
@@ -1,2 +1,5 @@
+# On linux, install powershell: dotnet tool install -g PowerShell
+# and run the script with: pwsh ./stryker.ps1
+
 # Install Stryker.NET globally: dotnet tool install -g dotnet-stryker
-dotnet-stryker -p src\TrendFox.Blazor.Regions\TrendFox.Blazor.Regions.csproj -tp test\TrendFox.Blazor.Regions.Tests\TrendFox.Blazor.Regions.Tests.csproj -o
+dotnet-stryker -p TrendFox.Blazor.Regions.csproj -tp $(Join-Path . test TrendFox.Blazor.Regions.Tests TrendFox.Blazor.Regions.Tests.csproj) -o
